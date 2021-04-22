7d8589dca98f8ae25406fa2f04185af29333bd47
Moez <moez_zouari@hotmail.fr>
2021-04-22 17:12:18 +0100

    handling page changes

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -2,17 +2,23 @@ import React from "react";
 //Convention because lodash is the optimized version of a popular JS library called underscore
 import _ from "lodash";
 const Pagination = (props) => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, onPageChange, currentPage } = props;
+  console.log(currentPage);
   const pagesCount = Math.ceil(itemsCount / pageSize);
-  console.log(pagesCount);
+
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
   return (
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className="page-item">
-            <a className="page-link">{page}</a>
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
           </li>
         ))}
       </ul>
