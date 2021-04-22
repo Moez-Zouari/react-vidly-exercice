ef3152fb2337a8c59f278964b28239b9cb84dbf2
Moez <moez_zouari@hotmail.fr>
2021-04-22 16:37:15 +0100

    displaying pages

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+//Convention because lodash is the optimized version of a popular JS library called underscore
+import _ from "lodash";
 const Pagination = (props) => {
-  return null;
+  const { itemsCount, pageSize } = props;
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  console.log(pagesCount);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li key={page} className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
