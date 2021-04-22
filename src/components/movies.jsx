c94818feaa8fef5ffe45a7e50ffbeb1cba7fd123
Moez <moez_zouari@hotmail.fr>
2021-04-22 15:03:18 +0100

    like and dislike movie exercice

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
   state = {
@@ -8,6 +9,19 @@ class Movies extends Component {
 
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = (movie) => {
+    // Cloner tous les movies
+    const movies = [...this.state.movies];
+    //Récupérer l'index de movie en parametre
+    const index = movies.indexOf(movie);
+    //Clonner le movie en paramétre a travers son index de puis le state
+    movies[index] = { ...movies[index] };
+    //Changer la valeur like par l'opposer de celle actuel
+    movies[index].liked = !movies[index].liked;
+    // Mettre a jour la vue
     this.setState({ movies });
   };
 
@@ -25,6 +39,8 @@ class Movies extends Component {
               <th>Genre</th>
               <th>Stock</th>
               <th>Rate</th>
+              <th />
+              <th />
             </tr>
           </thead>
           <tbody>
@@ -34,6 +50,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
