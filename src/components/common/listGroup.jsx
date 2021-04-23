4f36002c212d7ec89a0c9d45844be0d32b609e0d
Moez <moez_zouari@hotmail.fr>
2021-04-23 15:30:05 +0100

    displaying items

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const ListGroup = () => {
-  return null;
+const ListGroup = (props) => {
+  const { items, textProprety, valueProprety } = props;
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li key={item[valueProprety]} className="list-group-item">
+          {item[textProprety]}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListGroup;
