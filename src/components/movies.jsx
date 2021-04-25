f9878ee09788d4e7e4abe075201df62d5f5b5fc4
Moez <moez_zouari@hotmail.fr>
2021-04-25 15:30:10 +0100

    raising the sort event

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -16,7 +16,7 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
     this.setState({ movies: getMovies(), genres });
   }
 
@@ -44,6 +44,10 @@ class Movies extends Component {
 
   handleGenreSelect = (genre) => {
     this.setState({ selectedGenre: genre, currentPage: 1 });
+  };
+
+  handleSort = (path) => {
+    console.log(path);
   };
 
   render() {
@@ -80,6 +84,7 @@ class Movies extends Component {
             movies={movies}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
+            onSort={this.handleSort}
           />
 
           <Pagination
